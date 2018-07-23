35bba51cee6b7288c19f4dc125747184bdbd11dd
hamil168 <34875630+hamil168@users.noreply.github.com>
2018-07-23 00:04:26 -0700

    HR30DoC 16 - Exceptions Str to Int

---
+++ Misc Learning/HackerRank 30 Days of Code/16 - Exceptions Str to Int.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 22 23:59:13 2018
+
+@author: DRB4
+
+
+Task: read a string S, print its integer value else print 'Bad String'
+Must use try/except language. No conditionals allowed.
+
+constraints 1<= len(S) <= 6
+S is either lower case letters [a-z] or decimal digits [0-9]
+"""
+
+#!/bin/python3
+
+import sys
+
+S = input().strip()
+
+try:
+    int(S)
+    print(S)
+except ValueError:
+    print('Bad String')
+    
+# TEST CASES:
+# in '3' prints: '3' SUCCESS
+# in 'za' prints: 'Bad String' SUCCESS
+# in '3134' prints: '3134' SUCCESS
+# in 'abc' prints: 'Bad String" SUCCESS
